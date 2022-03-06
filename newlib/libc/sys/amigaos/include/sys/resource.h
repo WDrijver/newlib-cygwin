b1f56b9a9036ac1887e978ff7ddefea5f51d8a97
bebbo <bebbo@bejy.net>
2022-03-06 15:06:05 +0100

    add struct rlimit

--- newlib/libc/sys/amigaos/include/sys/resource.h
+++ newlib/libc/sys/amigaos/include/sys/resource.h
@@ -2,16 +2,24 @@
 #define _SYS_RESOURCE_H_
 
 #include <sys/time.h>
+#include <resource.h>
 
 #define	RUSAGE_SELF	0		/* calling process */
 #define	RUSAGE_CHILDREN	-1		/* terminated child processes */
 
 struct rusage {
-  	struct timeval ru_utime;	/* user time used */
-	struct timeval ru_stime;	/* system time used */
+	struct timeval ru_utime; /* user time used */
+	struct timeval ru_stime; /* system time used */
 };
 
-__stdargs int	getrusage (int, struct rusage*);
+typedef unsigned rlim_t;
+
+struct rlimit {
+	rlim_t rlim_cur; /* Soft limit */
+	rlim_t rlim_max; /* Hard limit (ceiling for rlim_cur) */
+};
+
+__stdargs int getrusage(int, struct rusage*);
+__stdargs int getrlimit(int resource, struct rlimit *rlim);
 
 #endif
-
